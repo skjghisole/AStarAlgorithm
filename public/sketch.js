1005b3a27aabc4954c73fc12789d170bdce7c4a8
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-09-09 00:30:32 -0400

    added sticky header

--- public/sketch.js
+++ public/sketch.js
@@ -13,6 +13,25 @@ let startDraw = false
 
 let isGuidelinesOn = document.getElementById('guidelines').checked
 
+window.onscroll = function() {
+	myFunction()
+};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 document.getElementById('start-button').addEventListener('click', function() {
 	startDrawing()
 })
